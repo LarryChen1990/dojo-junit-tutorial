2c168783a047ab96111b869f8bcf5b21bd38afd3
LarryChen1990 <larrychen1990@163.com>
2014-04-30 17:30:27 +0800

    1

--- src/test/java/com/citi/date/DateTest.java
+++ src/test/java/com/citi/date/DateTest.java
@@ -5,23 +5,26 @@ import java.util.Date;
 
 public class DateTest {
 	public static void main(String[] args) {
-		try {
-			Date date=new Date();
-			String f1="yyyy-dd-MM HH:mm:ss";
-			String f2="yyyy-dd-MM HH:mm:ss:S";
-			SimpleDateFormat sf=new SimpleDateFormat(f2);
-			
-			String s=sf.format(date);
-			
-			System.out.println(s);
-			Date date2=sf.parse(s);
-			System.out.println(date2);
-			
-			
-			
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+		Object o="";
+		System.out.println(o.toString().length());
+//		try {
+//			Date date=new Date();
+//			String f1="yyyy-dd-MM HH:mm:ss";
+//			String f2="yyyy-dd-MM HH:mm:ss:S";
+//			SimpleDateFormat sf=new SimpleDateFormat(f2);
+//			
+//			String s=sf.format(date);
+//			
+//			System.out.println(s);
+//			Date date2=sf.parse(s);
+//			System.out.println(date2);
+//			
+//			
+//			
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 }
